2cf3f066a7a4dbeff3b922067c538682cb0fa36c
nikola84459 <55811038+nikola84459@users.noreply.github.com>
2023-02-07 20:10:42 +0100

    Add files via upload

---
+++ 02-back-end/src/middleware/auth.middleware.ts
@@ -0,0 +1,90 @@
+import { NextFunction, Request, Response } from "express";
+import * as jwt from 'jsonwebtoken';
+import Config from '../config/dev';
+import ITokenData from '../components/auth/dto/ITokenData.interface';
+
+type UserRole = "korisnik" | "sluzbenik";
+
+interface TokenValidationInformation {
+    isValid: boolean;
+    data: any;
+}
+
+export default class AuthMiddleware {
+    
+    private static validacijaTokena(req: Request, res: Response, next: NextFunction, allowedRoles: UserRole[]) {
+        if(typeof req.headers.authorization !== "string") {
+            return res.status(401).send("Token nije validan.");
+        }
+
+        const token: string = req.headers.authorization;
+
+        
+        const [ tokenType, tokenString ] = token.trim().split(" ");
+
+        if (tokenType !== "Bearer") {
+            console.log("nije Bearer")
+            return res.status(401).send("Token nije validan.");
+        }
+
+                
+        if (typeof tokenString !== "string" || tokenString.length === 0) {
+            return res.status(401).send("Token nije validan.");
+        }
+
+        const korisnikValidacija = this.validateTokenByRole(tokenString, "korisnik");
+        const sluzbenikValidacija = this.validateTokenByRole(tokenString, "sluzbenik");
+
+        var rezultat;
+
+        if(korisnikValidacija.isValid === false && sluzbenikValidacija.isValid === false) {
+            console.log("korisnik")
+            return res.status(401).send("Token validation error: " + JSON.stringify(korisnikValidacija) + " " + JSON.stringify(sluzbenikValidacija));
+        }
+
+        if(korisnikValidacija.isValid) {
+            rezultat = korisnikValidacija.data;
+        } else {
+            rezultat = sluzbenikValidacija.data;
+        }
+
+        if(typeof rezultat !== "object") {
+            return res.status(401).send("Ne ispravni podaci tokena.");
+        }
+
+        const data: ITokenData = rezultat as ITokenData;
+
+        if(!allowedRoles.includes(data.role)) {
+            console.log("uloga")
+            return res.status(403).send("Pristup je odbijen za ovu ulogu korisnika.");
+        }
+
+        req.authorized = data;
+
+        next();
+    }
+
+    public static validateTokenByRole(tokenString: string, role: UserRole): TokenValidationInformation {
+        try {
+            const result = jwt.verify(tokenString, Config.auth[role].auth.public);
+            return {
+                isValid: true,
+                data: result
+            } 
+             
+        } catch(err) {
+            return {
+                isValid: false,
+                data: err?.message
+            }
+        }
+    }
+
+    public static getVerifier(
+        ... allowedRoles: UserRole[]
+    ): (req: Request, res: Response, next: NextFunction) => void {
+        return(req: Request, res: Response, next: NextFunction) => {
+            this.validacijaTokena(req, res, next, allowedRoles);
+        };
+    }
+}
